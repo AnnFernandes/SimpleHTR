efa97c19aaf1678881feb75f1f21cc5061eeb380
Ann Fernandes <ann.fernandes007@gmail.com>
2019-11-23 15:08:15 -0500

    Update DataLoader.py

--- src/DataLoader.py
+++ src/DataLoader.py
@@ -88,7 +88,7 @@ class DataLoader:
 		self.validationWords = [x.gtText for x in self.validationSamples]
 
 		# number of randomly chosen samples per epoch for training 
-		self.numTrainSamplesPerEpoch = 25000 
+		self.numTrainSamplesPerEpoch = 100 
 		
 		# start with train set
 		self.trainSet()
