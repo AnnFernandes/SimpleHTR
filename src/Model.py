bdfa0012a9095d95d348187a7a5677dd7aa49487
Ann Fernandes <ann.fernandes007@gmail.com>
2019-11-23 00:25:16 -0500

    Update Model.py

--- src/Model.py
+++ src/Model.py
@@ -133,7 +133,6 @@ class Model:
 
 
 	def setupRNN(self):
-		
 		"create RNN layers and return output of these layers"
 		rnnIn4d = tf.slice(rnnIn4d, [0, 0, 0, 0], [self.batchSize, 100, 1, 512])
         	rnnIn3d = tf.squeeze(rnnIn4d)
