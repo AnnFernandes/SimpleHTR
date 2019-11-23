b90e4098cf4bd535a50b565743348e5147c5ba1c
Ann Fernandes <ann.fernandes007@gmail.com>
2019-11-23 13:21:43 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ import numpy as np
 import tensorflow as tf
 
 from DataLoader import Batch, DataLoader, FilePaths
-from helpers import preprocessor, wer
+from SamplePreprocessor import preprocessor, wer
 from Model import DecoderType, Model
 
 # # Disable GPU
