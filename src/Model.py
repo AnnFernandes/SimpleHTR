88156600b4706eac6004b31204358a19943eb0ae
Ann Fernandes <ann.fernandes007@gmail.com>
2019-11-23 01:12:03 -0500

    Update Model.py

--- src/Model.py
+++ src/Model.py
@@ -177,16 +177,16 @@ class Model:
         with tf.name_scope('CTC_Decoder'):
             # Decoder: Best path decoding or Word beam search decoding
             if self.decoderType == DecoderType.BestPath:
-			decoder = tf.nn.ctc_greedy_decoder(inputs=ctcIn3dTBC, sequence_length=self.seqLen)
+		decoder = tf.nn.ctc_greedy_decoder(inputs=ctcIn3dTBC, sequence_length=self.seqLen)
             elif self.decoderType == DecoderType.WordBeamSearch:
-                	# Import compiled word beam search operation (see https://github.com/githubharald/CTCWordBeamSearch)
-                	word_beam_search_module = tf.load_op_library('./TFWordBeamSearch.so')
-			chars = str().join(self.charList)
-			wordChars = open('../model/wordCharList.txt').read().splitlines()[0]
-			corpus = open('../data/corpus.txt').read()
+                # Import compiled word beam search operation (see https://github.com/githubharald/CTCWordBeamSearch)
+                word_beam_search_module = tf.load_op_library('./TFWordBeamSearch.so')
+		chars = str().join(self.charList)
+		wordChars = open('../model/wordCharList.txt').read().splitlines()[0]
+		corpus = open('../data/corpus.txt').read()
 
-			# decode using the "Words" mode of word beam search
-			self.decoder = word_beam_search_module.word_beam_search(tf.nn.softmax(self.ctcIn3dTBC, dim=2), 50, 'Words', 0.0, corpus.encode('utf8'), chars.encode('utf8'), wordChars.encode('utf8'))
+		# decode using the "Words" mode of word beam search
+		self.decoder = word_beam_search_module.word_beam_search(tf.nn.softmax(self.ctcIn3dTBC, dim=2), 50, 'Words', 0.0, corpus.encode('utf8'), chars.encode('utf8'), wordChars.encode('utf8'))
 		
         # Return a CTC operation to compute the loss and CTC operation to decode the RNN output
         return (tf.reduce_mean(loss), decoder)
